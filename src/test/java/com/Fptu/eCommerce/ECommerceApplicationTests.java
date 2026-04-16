62a8322c55443beb3f5dd34573dc463c6f9abc0e
KhacToanNguyen-23 <khactoan2233@gmail.com>
2026-04-16 23:59:04 +0700

    update project

---
+++ src/test/java/com/Fptu/eCommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fptu.eCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
